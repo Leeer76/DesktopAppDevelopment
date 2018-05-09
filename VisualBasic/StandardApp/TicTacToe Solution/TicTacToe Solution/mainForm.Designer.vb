2e2ac863695d99af4e22c693a1980db709e460af
Erik <34690908+Leeer76@users.noreply.github.com>
2018-05-09 11:40:38 -0500

    Add files via upload

---
+++ VisualBasic/StandardApp/TicTacToe Solution/TicTacToe Solution/mainForm.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class mainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.newButton = New System.Windows.Forms.Button()
+        Me.leftTopLabel = New System.Windows.Forms.Label()
+        Me.leftMiddleLabel = New System.Windows.Forms.Label()
+        Me.leftBottomLabel = New System.Windows.Forms.Label()
+        Me.middleTopLabel = New System.Windows.Forms.Label()
+        Me.middleMiddleLabel = New System.Windows.Forms.Label()
+        Me.middleBottomLabel = New System.Windows.Forms.Label()
+        Me.rightTopLabel = New System.Windows.Forms.Label()
+        Me.rightMiddleLabel = New System.Windows.Forms.Label()
+        Me.rightBottomLabel = New System.Windows.Forms.Label()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.mainTableLayoutPanel = New System.Windows.Forms.TableLayoutPanel()
+        Me.mainTableLayoutPanel.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'newButton
+        '
+        Me.newButton.Location = New System.Drawing.Point(48, 269)
+        Me.newButton.Name = "newButton"
+        Me.newButton.Size = New System.Drawing.Size(104, 29)
+        Me.newButton.TabIndex = 0
+        Me.newButton.Text = "&New Game"
+        Me.newButton.UseVisualStyleBackColor = True
+        '
+        'leftTopLabel
+        '
+        Me.leftTopLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.leftTopLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.leftTopLabel.Location = New System.Drawing.Point(13, 10)
+        Me.leftTopLabel.Name = "leftTopLabel"
+        Me.leftTopLabel.Size = New System.Drawing.Size(53, 46)
+        Me.leftTopLabel.TabIndex = 1
+        Me.leftTopLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'leftMiddleLabel
+        '
+        Me.leftMiddleLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.leftMiddleLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.leftMiddleLabel.Location = New System.Drawing.Point(13, 77)
+        Me.leftMiddleLabel.Name = "leftMiddleLabel"
+        Me.leftMiddleLabel.Size = New System.Drawing.Size(53, 46)
+        Me.leftMiddleLabel.TabIndex = 2
+        Me.leftMiddleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'leftBottomLabel
+        '
+        Me.leftBottomLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.leftBottomLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.leftBottomLabel.Location = New System.Drawing.Point(13, 144)
+        Me.leftBottomLabel.Name = "leftBottomLabel"
+        Me.leftBottomLabel.Size = New System.Drawing.Size(53, 46)
+        Me.leftBottomLabel.TabIndex = 3
+        Me.leftBottomLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'middleTopLabel
+        '
+        Me.middleTopLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.middleTopLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.middleTopLabel.Location = New System.Drawing.Point(93, 10)
+        Me.middleTopLabel.Name = "middleTopLabel"
+        Me.middleTopLabel.Size = New System.Drawing.Size(53, 46)
+        Me.middleTopLabel.TabIndex = 4
+        Me.middleTopLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'middleMiddleLabel
+        '
+        Me.middleMiddleLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.middleMiddleLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.middleMiddleLabel.Location = New System.Drawing.Point(93, 77)
+        Me.middleMiddleLabel.Name = "middleMiddleLabel"
+        Me.middleMiddleLabel.Size = New System.Drawing.Size(53, 46)
+        Me.middleMiddleLabel.TabIndex = 5
+        Me.middleMiddleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'middleBottomLabel
+        '
+        Me.middleBottomLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.middleBottomLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.middleBottomLabel.Location = New System.Drawing.Point(93, 144)
+        Me.middleBottomLabel.Name = "middleBottomLabel"
+        Me.middleBottomLabel.Size = New System.Drawing.Size(53, 46)
+        Me.middleBottomLabel.TabIndex = 6
+        Me.middleBottomLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'rightTopLabel
+        '
+        Me.rightTopLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.rightTopLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.rightTopLabel.Location = New System.Drawing.Point(173, 10)
+        Me.rightTopLabel.Name = "rightTopLabel"
+        Me.rightTopLabel.Size = New System.Drawing.Size(53, 46)
+        Me.rightTopLabel.TabIndex = 7
+        Me.rightTopLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'rightMiddleLabel
+        '
+        Me.rightMiddleLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.rightMiddleLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.rightMiddleLabel.Location = New System.Drawing.Point(173, 77)
+        Me.rightMiddleLabel.Name = "rightMiddleLabel"
+        Me.rightMiddleLabel.Size = New System.Drawing.Size(53, 46)
+        Me.rightMiddleLabel.TabIndex = 8
+        Me.rightMiddleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'rightBottomLabel
+        '
+        Me.rightBottomLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.rightBottomLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.rightBottomLabel.Location = New System.Drawing.Point(173, 144)
+        Me.rightBottomLabel.Name = "rightBottomLabel"
+        Me.rightBottomLabel.Size = New System.Drawing.Size(53, 46)
+        Me.rightBottomLabel.TabIndex = 9
+        Me.rightBottomLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'exitButton
+        '
+        Me.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.exitButton.Location = New System.Drawing.Point(158, 269)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(104, 29)
+        Me.exitButton.TabIndex = 1
+        Me.exitButton.Text = "E&xit"
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'mainTableLayoutPanel
+        '
+        Me.mainTableLayoutPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.mainTableLayoutPanel.ColumnCount = 3
+        Me.mainTableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.mainTableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.mainTableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.mainTableLayoutPanel.Controls.Add(Me.rightBottomLabel, 2, 2)
+        Me.mainTableLayoutPanel.Controls.Add(Me.rightMiddleLabel, 2, 1)
+        Me.mainTableLayoutPanel.Controls.Add(Me.rightTopLabel, 2, 0)
+        Me.mainTableLayoutPanel.Controls.Add(Me.leftMiddleLabel, 0, 1)
+        Me.mainTableLayoutPanel.Controls.Add(Me.middleBottomLabel, 1, 2)
+        Me.mainTableLayoutPanel.Controls.Add(Me.leftBottomLabel, 0, 2)
+        Me.mainTableLayoutPanel.Controls.Add(Me.middleMiddleLabel, 1, 1)
+        Me.mainTableLayoutPanel.Controls.Add(Me.leftTopLabel, 0, 0)
+        Me.mainTableLayoutPanel.Controls.Add(Me.middleTopLabel, 1, 0)
+        Me.mainTableLayoutPanel.Location = New System.Drawing.Point(35, 34)
+        Me.mainTableLayoutPanel.Name = "mainTableLayoutPanel"
+        Me.mainTableLayoutPanel.RowCount = 3
+        Me.mainTableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.mainTableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.mainTableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.mainTableLayoutPanel.Size = New System.Drawing.Size(240, 201)
+        Me.mainTableLayoutPanel.TabIndex = 2
+        '
+        'mainForm
+        '
+        Me.AcceptButton = Me.newButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.CancelButton = Me.exitButton
+        Me.ClientSize = New System.Drawing.Size(310, 321)
+        Me.Controls.Add(Me.mainTableLayoutPanel)
+        Me.Controls.Add(Me.exitButton)
+        Me.Controls.Add(Me.newButton)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "mainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tic Tac Toe"
+        Me.mainTableLayoutPanel.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents newButton As Button
+    Friend WithEvents leftTopLabel As Label
+    Friend WithEvents rightBottomLabel As Label
+    Friend WithEvents rightMiddleLabel As Label
+    Friend WithEvents rightTopLabel As Label
+    Friend WithEvents middleBottomLabel As Label
+    Friend WithEvents middleMiddleLabel As Label
+    Friend WithEvents middleTopLabel As Label
+    Friend WithEvents leftBottomLabel As Label
+    Friend WithEvents leftMiddleLabel As Label
+    Friend WithEvents exitButton As Button
+    Friend WithEvents mainTableLayoutPanel As TableLayoutPanel
+End Class
---
+++ VisualBasic/StandardApp/TicTacToe Solution/TicTacToe Solution/mainForm.vb
@@ -0,0 +1,157 @@
+﻿' Project name:         TicTacToe Solution
+' Project purpose:      simulate a game of tic tac toe
+' Created/revised by:   Erik Lee on Mar 22, 2017
+
+Option Strict On
+Option Infer Off
+Option Explicit On
+
+Public Class mainForm
+    'declare variable
+    Dim gameBoard() As Label = {New Label, New Label, New Label, New Label, New Label, New Label, New Label, New Label, New Label}
+    Dim isWinner As Boolean
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        'close application
+        Dim response As Integer
+        response = MessageBox.Show("Do you want to quit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If response = vbYes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub newButton_Click(sender As Object, e As EventArgs) Handles newButton.Click
+        'clears fields and to creat new game
+        mainTableLayoutPanel.Enabled = True
+        leftTopLabel.Text = String.Empty
+        leftTopLabel.Enabled = True
+        leftTopLabel.BackColor = DefaultBackColor
+        leftMiddleLabel.Text = String.Empty
+        leftMiddleLabel.Enabled = True
+        leftMiddleLabel.BackColor = DefaultBackColor
+        leftBottomLabel.Text = String.Empty
+        leftBottomLabel.Enabled = True
+        leftBottomLabel.BackColor = DefaultBackColor
+        middleTopLabel.Text = String.Empty
+        middleTopLabel.Enabled = True
+        middleTopLabel.BackColor = DefaultBackColor
+        middleMiddleLabel.Text = String.Empty
+        middleMiddleLabel.Enabled = True
+        middleMiddleLabel.BackColor = DefaultBackColor
+        middleBottomLabel.Text = String.Empty
+        middleBottomLabel.Enabled = True
+        middleBottomLabel.BackColor = DefaultBackColor
+        rightTopLabel.Text = String.Empty
+        rightTopLabel.Enabled = True
+        rightTopLabel.BackColor = DefaultBackColor
+        rightMiddleLabel.Text = String.Empty
+        rightMiddleLabel.Enabled = True
+        rightMiddleLabel.BackColor = DefaultBackColor
+        rightBottomLabel.Text = String.Empty
+        rightBottomLabel.Enabled = True
+        rightBottomLabel.BackColor = DefaultBackColor
+        gameBoard(0).Text = String.Empty
+        gameBoard(1).Text = String.Empty
+        gameBoard(2).Text = String.Empty
+        gameBoard(3).Text = String.Empty
+        gameBoard(4).Text = String.Empty
+        gameBoard(5).Text = String.Empty
+        gameBoard(6).Text = String.Empty
+        gameBoard(7).Text = String.Empty
+        gameBoard(8).Text = String.Empty
+        isWinner = False
+
+
+    End Sub
+
+    Private Sub Winner(sender As Object, e As EventArgs) Handles leftTopLabel.Click, leftMiddleLabel.Click, leftBottomLabel.Click, middleTopLabel.Click, middleMiddleLabel.Click, middleBottomLabel.Click, rightTopLabel.Click, rightMiddleLabel.Click, rightBottomLabel.Click
+        'determine the winner
+        'declare variables
+        Dim currentLable As Label
+        Dim subscript As Integer
+        Static player As String = "X"
+        Static squareColor As Color = Color.LightGreen
+
+        'convert sender to label
+        currentLable = TryCast(sender, Label)
+        'declares x or o in label
+        currentLable.Text = player
+        'changes back color
+        currentLable.BackColor = squareColor
+        'does not allow use of the label again
+        currentLable.Enabled = False
+
+        Select Case currentLable.Name
+            Case "leftTopLabel"
+                subscript = 0
+            Case "leftMiddleLabel"
+                subscript = 1
+            Case "leftBottomLabel"
+                subscript = 2
+            Case "middleTopLabel"
+                subscript = 3
+            Case "middleMiddleLabel"
+                subscript = 4
+            Case "middleBottomLabel"
+                subscript = 5
+            Case "rightTopLabel"
+                subscript = 6
+            Case "rightMiddleLabel"
+                subscript = 7
+            Case "rightBottomLabel"
+                subscript = 8
+        End Select
+
+        'enter array data
+        gameBoard(subscript).Text = player
+
+        Select Case player & player & player
+            Case gameBoard(0).Text & gameBoard(1).Text & gameBoard(2).Text
+                isWinner = True
+            Case gameBoard(3).Text & gameBoard(4).Text & gameBoard(5).Text
+                isWinner = True
+            Case gameBoard(6).Text & gameBoard(7).Text & gameBoard(8).Text
+                isWinner = True
+            Case gameBoard(0).Text & gameBoard(3).Text & gameBoard(6).Text
+                isWinner = True
+            Case gameBoard(1).Text & gameBoard(4).Text & gameBoard(7).Text
+                isWinner = True
+            Case gameBoard(2).Text & gameBoard(5).Text & gameBoard(8).Text
+                isWinner = True
+            Case gameBoard(0).Text & gameBoard(4).Text & gameBoard(8).Text
+                isWinner = True
+            Case gameBoard(6).Text & gameBoard(4).Text & gameBoard(2).Text
+                isWinner = True
+        End Select
+
+        If isWinner Then
+            MessageBox.Show("Game over! Player " & player & " is the winner", "End of Game", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            mainTableLayoutPanel.Enabled = False
+        ElseIf (TIETEST()) Then
+            MessageBox.Show("Stalemate, No Winners!", "Tie", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            newButton.PerformClick()
+        Else
+            If player = "X" Then
+                player = "O"
+                squareColor = Color.LightPink
+            Else
+                player = "X"
+                squareColor = Color.LightGreen
+            End If
+        End If
+
+
+    End Sub
+
+    Private Function TIETEST() As Boolean
+        'test if a tie has happened
+        For i As Integer = 0 To gameBoard.GetUpperBound(0)
+            If (Not (gameBoard(i).Text = "X" OrElse gameBoard(i).Text = "O")) Then
+                Return False
+            End If
+        Next
+        Return True
+    End Function
+
+End Class

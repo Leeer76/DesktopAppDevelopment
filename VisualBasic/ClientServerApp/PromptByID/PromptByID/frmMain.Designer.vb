3102a9e62dcd0b7543722f8413344c6fe60058c8
Erik <34690908+Leeer76@users.noreply.github.com>
2018-05-09 11:53:41 -0500

    Add files via upload

---
+++ VisualBasic/ClientServerApp/PromptByID/PromptByID/frmMain.Designer.vb
@@ -0,0 +1,328 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim IDLabel As System.Windows.Forms.Label
+        Dim LastNameLabel As System.Windows.Forms.Label
+        Dim FirstNameLabel As System.Windows.Forms.Label
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.CIS272BindingNavigator = New System.Windows.Forms.BindingNavigator(Me.components)
+        Me.BindingNavigatorAddNewItem = New System.Windows.Forms.ToolStripButton()
+        Me.CIS272BindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CIS272DataSet = New PromptByID.CIS272DataSet()
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
+        Me.BindingNavigatorDeleteItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMoveFirstItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
+        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorMoveLastItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.CIS272BindingNavigatorSaveItem = New System.Windows.Forms.ToolStripButton()
+        Me.FillToolStrip = New System.Windows.Forms.ToolStrip()
+        Me.IDToolStripLabel = New System.Windows.Forms.ToolStripLabel()
+        Me.IDToolStripTextBox = New System.Windows.Forms.ToolStripTextBox()
+        Me.FillToolStripButton = New System.Windows.Forms.ToolStripButton()
+        Me.IDTextBox = New System.Windows.Forms.TextBox()
+        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.CIS272TableAdapter = New PromptByID.CIS272DataSetTableAdapters.CIS272TableAdapter()
+        Me.TableAdapterManager = New PromptByID.CIS272DataSetTableAdapters.TableAdapterManager()
+        IDLabel = New System.Windows.Forms.Label()
+        LastNameLabel = New System.Windows.Forms.Label()
+        FirstNameLabel = New System.Windows.Forms.Label()
+        CType(Me.CIS272BindingNavigator, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.CIS272BindingNavigator.SuspendLayout()
+        CType(Me.CIS272BindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CIS272DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.FillToolStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'IDLabel
+        '
+        IDLabel.AutoSize = True
+        IDLabel.Location = New System.Drawing.Point(12, 73)
+        IDLabel.Name = "IDLabel"
+        IDLabel.Size = New System.Drawing.Size(25, 17)
+        IDLabel.TabIndex = 1
+        IDLabel.Text = "ID:"
+        '
+        'LastNameLabel
+        '
+        LastNameLabel.AutoSize = True
+        LastNameLabel.Location = New System.Drawing.Point(12, 101)
+        LastNameLabel.Name = "LastNameLabel"
+        LastNameLabel.Size = New System.Drawing.Size(80, 17)
+        LastNameLabel.TabIndex = 3
+        LastNameLabel.Text = "Last Name:"
+        '
+        'FirstNameLabel
+        '
+        FirstNameLabel.AutoSize = True
+        FirstNameLabel.Location = New System.Drawing.Point(12, 129)
+        FirstNameLabel.Name = "FirstNameLabel"
+        FirstNameLabel.Size = New System.Drawing.Size(80, 17)
+        FirstNameLabel.TabIndex = 5
+        FirstNameLabel.Text = "First Name:"
+        '
+        'CIS272BindingNavigator
+        '
+        Me.CIS272BindingNavigator.AddNewItem = Me.BindingNavigatorAddNewItem
+        Me.CIS272BindingNavigator.BindingSource = Me.CIS272BindingSource
+        Me.CIS272BindingNavigator.CountItem = Me.BindingNavigatorCountItem
+        Me.CIS272BindingNavigator.DeleteItem = Me.BindingNavigatorDeleteItem
+        Me.CIS272BindingNavigator.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.CIS272BindingNavigator.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMoveFirstItem, Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.BindingNavigatorSeparator1, Me.BindingNavigatorMoveNextItem, Me.BindingNavigatorMoveLastItem, Me.BindingNavigatorSeparator2, Me.BindingNavigatorAddNewItem, Me.BindingNavigatorDeleteItem, Me.CIS272BindingNavigatorSaveItem})
+        Me.CIS272BindingNavigator.Location = New System.Drawing.Point(0, 0)
+        Me.CIS272BindingNavigator.MoveFirstItem = Me.BindingNavigatorMoveFirstItem
+        Me.CIS272BindingNavigator.MoveLastItem = Me.BindingNavigatorMoveLastItem
+        Me.CIS272BindingNavigator.MoveNextItem = Me.BindingNavigatorMoveNextItem
+        Me.CIS272BindingNavigator.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
+        Me.CIS272BindingNavigator.Name = "CIS272BindingNavigator"
+        Me.CIS272BindingNavigator.PositionItem = Me.BindingNavigatorPositionItem
+        Me.CIS272BindingNavigator.Size = New System.Drawing.Size(475, 27)
+        Me.CIS272BindingNavigator.TabIndex = 7
+        Me.CIS272BindingNavigator.Text = "BindingNavigator1"
+        '
+        'BindingNavigatorAddNewItem
+        '
+        Me.BindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorAddNewItem.Image = CType(resources.GetObject("BindingNavigatorAddNewItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorAddNewItem.Name = "BindingNavigatorAddNewItem"
+        Me.BindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorAddNewItem.Size = New System.Drawing.Size(24, 24)
+        Me.BindingNavigatorAddNewItem.Text = "Add new"
+        '
+        'CIS272BindingSource
+        '
+        Me.CIS272BindingSource.DataMember = "CIS272"
+        Me.CIS272BindingSource.DataSource = Me.CIS272DataSet
+        '
+        'CIS272DataSet
+        '
+        Me.CIS272DataSet.DataSetName = "CIS272DataSet"
+        Me.CIS272DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(45, 24)
+        Me.BindingNavigatorCountItem.Text = "of {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorDeleteItem
+        '
+        Me.BindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorDeleteItem.Image = CType(resources.GetObject("BindingNavigatorDeleteItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorDeleteItem.Name = "BindingNavigatorDeleteItem"
+        Me.BindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorDeleteItem.Size = New System.Drawing.Size(24, 24)
+        Me.BindingNavigatorDeleteItem.Text = "Delete"
+        '
+        'BindingNavigatorMoveFirstItem
+        '
+        Me.BindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveFirstItem.Image = CType(resources.GetObject("BindingNavigatorMoveFirstItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveFirstItem.Name = "BindingNavigatorMoveFirstItem"
+        Me.BindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveFirstItem.Size = New System.Drawing.Size(24, 24)
+        Me.BindingNavigatorMoveFirstItem.Text = "Move first"
+        '
+        'BindingNavigatorMovePreviousItem
+        '
+        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
+        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(24, 24)
+        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
+        '
+        'BindingNavigatorSeparator
+        '
+        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 27)
+        '
+        'BindingNavigatorPositionItem
+        '
+        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
+        Me.BindingNavigatorPositionItem.AutoSize = False
+        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
+        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 27)
+        Me.BindingNavigatorPositionItem.Text = "0"
+        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
+        '
+        'BindingNavigatorSeparator1
+        '
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator1"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 27)
+        '
+        'BindingNavigatorMoveNextItem
+        '
+        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
+        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(24, 24)
+        Me.BindingNavigatorMoveNextItem.Text = "Move next"
+        '
+        'BindingNavigatorMoveLastItem
+        '
+        Me.BindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveLastItem.Image = CType(resources.GetObject("BindingNavigatorMoveLastItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveLastItem.Name = "BindingNavigatorMoveLastItem"
+        Me.BindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveLastItem.Size = New System.Drawing.Size(24, 24)
+        Me.BindingNavigatorMoveLastItem.Text = "Move last"
+        '
+        'BindingNavigatorSeparator2
+        '
+        Me.BindingNavigatorSeparator2.Name = "BindingNavigatorSeparator2"
+        Me.BindingNavigatorSeparator2.Size = New System.Drawing.Size(6, 27)
+        '
+        'CIS272BindingNavigatorSaveItem
+        '
+        Me.CIS272BindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.CIS272BindingNavigatorSaveItem.Image = CType(resources.GetObject("CIS272BindingNavigatorSaveItem.Image"), System.Drawing.Image)
+        Me.CIS272BindingNavigatorSaveItem.Name = "CIS272BindingNavigatorSaveItem"
+        Me.CIS272BindingNavigatorSaveItem.Size = New System.Drawing.Size(24, 24)
+        Me.CIS272BindingNavigatorSaveItem.Text = "Save Data"
+        '
+        'FillToolStrip
+        '
+        Me.FillToolStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.FillToolStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.IDToolStripLabel, Me.IDToolStripTextBox, Me.FillToolStripButton})
+        Me.FillToolStrip.Location = New System.Drawing.Point(0, 27)
+        Me.FillToolStrip.Name = "FillToolStrip"
+        Me.FillToolStrip.Size = New System.Drawing.Size(475, 27)
+        Me.FillToolStrip.TabIndex = 0
+        Me.FillToolStrip.TabStop = True
+        Me.FillToolStrip.Text = "FillToolStrip"
+        '
+        'IDToolStripLabel
+        '
+        Me.IDToolStripLabel.Name = "IDToolStripLabel"
+        Me.IDToolStripLabel.Size = New System.Drawing.Size(27, 24)
+        Me.IDToolStripLabel.Text = "ID:"
+        '
+        'IDToolStripTextBox
+        '
+        Me.IDToolStripTextBox.Name = "IDToolStripTextBox"
+        Me.IDToolStripTextBox.Size = New System.Drawing.Size(100, 27)
+        '
+        'FillToolStripButton
+        '
+        Me.FillToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.FillToolStripButton.Name = "FillToolStripButton"
+        Me.FillToolStripButton.Size = New System.Drawing.Size(32, 24)
+        Me.FillToolStripButton.Text = "Fill"
+        '
+        'IDTextBox
+        '
+        Me.IDTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CIS272BindingSource, "ID", True))
+        Me.IDTextBox.Location = New System.Drawing.Point(98, 70)
+        Me.IDTextBox.Name = "IDTextBox"
+        Me.IDTextBox.Size = New System.Drawing.Size(337, 22)
+        Me.IDTextBox.TabIndex = 2
+        '
+        'LastNameTextBox
+        '
+        Me.LastNameTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CIS272BindingSource, "LastName", True))
+        Me.LastNameTextBox.Location = New System.Drawing.Point(98, 98)
+        Me.LastNameTextBox.Name = "LastNameTextBox"
+        Me.LastNameTextBox.Size = New System.Drawing.Size(337, 22)
+        Me.LastNameTextBox.TabIndex = 4
+        '
+        'FirstNameTextBox
+        '
+        Me.FirstNameTextBox.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.CIS272BindingSource, "FirstName", True))
+        Me.FirstNameTextBox.Location = New System.Drawing.Point(98, 126)
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        Me.FirstNameTextBox.Size = New System.Drawing.Size(337, 22)
+        Me.FirstNameTextBox.TabIndex = 6
+        '
+        'CIS272TableAdapter
+        '
+        Me.CIS272TableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.CIS272TableAdapter = Me.CIS272TableAdapter
+        Me.TableAdapterManager.UpdateOrder = PromptByID.CIS272DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(475, 222)
+        Me.Controls.Add(IDLabel)
+        Me.Controls.Add(Me.IDTextBox)
+        Me.Controls.Add(LastNameLabel)
+        Me.Controls.Add(Me.LastNameTextBox)
+        Me.Controls.Add(FirstNameLabel)
+        Me.Controls.Add(Me.FirstNameTextBox)
+        Me.Controls.Add(Me.FillToolStrip)
+        Me.Controls.Add(Me.CIS272BindingNavigator)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Name Lookup"
+        CType(Me.CIS272BindingNavigator, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.CIS272BindingNavigator.ResumeLayout(False)
+        Me.CIS272BindingNavigator.PerformLayout()
+        CType(Me.CIS272BindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CIS272DataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.FillToolStrip.ResumeLayout(False)
+        Me.FillToolStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CIS272DataSet As CIS272DataSet
+    Friend WithEvents CIS272BindingSource As BindingSource
+    Friend WithEvents CIS272TableAdapter As CIS272DataSetTableAdapters.CIS272TableAdapter
+    Friend WithEvents TableAdapterManager As CIS272DataSetTableAdapters.TableAdapterManager
+    Friend WithEvents CIS272BindingNavigator As BindingNavigator
+    Friend WithEvents BindingNavigatorAddNewItem As ToolStripButton
+    Friend WithEvents BindingNavigatorCountItem As ToolStripLabel
+    Friend WithEvents BindingNavigatorDeleteItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveFirstItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMovePreviousItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator As ToolStripSeparator
+    Friend WithEvents BindingNavigatorPositionItem As ToolStripTextBox
+    Friend WithEvents BindingNavigatorSeparator1 As ToolStripSeparator
+    Friend WithEvents BindingNavigatorMoveNextItem As ToolStripButton
+    Friend WithEvents BindingNavigatorMoveLastItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator2 As ToolStripSeparator
+    Friend WithEvents CIS272BindingNavigatorSaveItem As ToolStripButton
+    Friend WithEvents FillToolStrip As ToolStrip
+    Friend WithEvents IDToolStripLabel As ToolStripLabel
+    Friend WithEvents IDToolStripTextBox As ToolStripTextBox
+    Friend WithEvents FillToolStripButton As ToolStripButton
+    Friend WithEvents IDTextBox As TextBox
+    Friend WithEvents LastNameTextBox As TextBox
+    Friend WithEvents FirstNameTextBox As TextBox
+End Class
---
+++ VisualBasic/ClientServerApp/PromptByID/PromptByID/frmMain.vb
@@ -0,0 +1,25 @@
+﻿Public Class frmMain
+    Private Sub CIS272BindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles CIS272BindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.CIS272BindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.CIS272DataSet)
+
+    End Sub
+
+    Private Sub FillToolStripButton_Click(sender As Object, e As EventArgs) Handles FillToolStripButton.Click
+        Try
+            Me.CIS272TableAdapter.Fill(Me.CIS272DataSet.CIS272, New System.Nullable(Of Integer)(CType(IDToolStripTextBox.Text, Integer)))
+        Catch ex As System.Exception
+            System.Windows.Forms.MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub IDToolStripTextBox_KeyDown(sender As Object, e As KeyEventArgs) Handles IDToolStripTextBox.KeyDown
+        'if enter was pressed call the fill click event
+        If e.KeyCode = Keys.Enter Then
+            IDToolStripTextBox.Text = IDToolStripTextBox.Text.Replace(";", "").Replace("?", "")
+            FillToolStripButton.PerformClick()
+        End If
+    End Sub
+End Class

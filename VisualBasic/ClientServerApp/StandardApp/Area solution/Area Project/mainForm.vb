7a9baa4fd4b0b80d5ff2b1e435f41fbc43afbab0
Erik <34690908+Leeer76@users.noreply.github.com>
2018-05-09 11:11:30 -0500

    Add files via upload

---
+++ VisualBasic/ClientServerApp/StandardApp/Area solution/Area Project/mainForm.vb
@@ -0,0 +1,54 @@
+﻿' Programmer: Erik Lee
+' Date: Sept 1, 2016
+' Program: Area Formulas
+' Purpose: Display the area formulas
+
+Public Class mainForm
+    Private Sub circleButton_Click(sender As Object, e As EventArgs) Handles circleButton.Click
+
+        'display formula for circle
+        formulaLabel.Text = "Area = π × r(sq)"
+
+        'displays image of circle
+        displayPictureBox.Image = circlePictureBox.Image
+
+
+    End Sub
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+
+        'exit application
+        Me.Close()
+
+    End Sub
+
+    Private Sub paralleButton_Click(sender As Object, e As EventArgs) Handles paralleButton.Click
+
+        'display formula for parallelogram
+        formulaLabel.Text = "Area = b × h"
+
+        'display image of parallelogram
+        displayPictureBox.Image = parallePictureBox.Image
+
+    End Sub
+
+    Private Sub rectangleButton_Click(sender As Object, e As EventArgs) Handles rectangleButton.Click
+
+        'display formula for rectangle
+        formulaLabel.Text = "Area = w × h"
+
+        'display image of rectangle
+        displayPictureBox.Image = rectanglePictureBox.Image
+
+    End Sub
+
+    Private Sub triangleButton_Click(sender As Object, e As EventArgs) Handles triangleButton.Click
+
+        'display formula for triangle
+        formulaLabel.Text = "Area = ½ × b × h"
+
+        'display image of triangle
+        displayPictureBox.Image = trianglePictureBox.Image
+
+    End Sub
+End Class
